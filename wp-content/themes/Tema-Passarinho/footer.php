12a10e0c41cda2f1c634d4c0b1f700a5116fc095
Ederson Walendorff <contato@edersonwalendorff.com>
2023-11-05 22:50:03 -0300

    Subindo o tema personalizado, e configurando php padrão do wordpress

---
+++ wp-content/themes/Tema-Passarinho/footer.php
@@ -0,0 +1,40 @@
+    <!-- footer -->
+    <footer>
+        <!-- conteudo_footer -->
+        <section class="conteudo_footer">
+            <!-- conteudo_footer--menu-footer -->
+            <div class="conteudo_footer--menu-footer">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Passarinhos</a>
+                    </li>
+                    <li>
+                        <a href="">Cadastrar passarinhos</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- conteudo_footer--menu-footer -->
+            <div class="conteudo_foote--rede-sociais">
+                <h5>Siga-nos nas redes sociais</h5>
+                <ul>
+                    <li>
+                        <a href=""><i class="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-brands fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-brands fa-youtube"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <h4>todos os direitos reservados</h4>
+        <!-- conteudo_footer -->
+    </footer>
+    <!-- footer -->
+</body>
+</html>
---
+++ wp-content/themes/Tema-Passarinho/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <title>Teste para programador WordPress</title>
+    <link rel="icon" href="<?php echo get_site_icon_url(); ?>" sizes="32x32" />
+    <link rel="stylesheet" href="<?php echo get_theme_root_uri(); ?>/Tema-Passarinho/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body>
+    <!-- topo -->
+        <section class="topo">
+            <!-- Center -->
+            <div class="center">
+                <!-- Header -->
+                <header>
+                    <!-- logo -->
+                    <div class="logo">
+                        <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_root_uri(); ?>/Tema-Passarinho/images/logo.svg" alt="logo do site"></a>
+                    </div>
+                    <!-- logo -->
+                    <nav>
+                        <!-- menu-desktop -->
+                        <ul class="menu-desktop">
+                            <li>
+                                <a href="<?php echo home_url(); ?>">Home</a>
+                            </li>
+                            <li>
+                                <a href="">Passarinhos</a>
+                            </li>
+                            <li>
+                                <a href="">Cadastrar passarinhos</a>
+                            </li>
+                        </ul>
+                        <!-- menu-desktop -->
+                    </nav>
+                </header>
+                <br>
+                <br>
+                <br>
+                <br>
+                <!-- Chamada -->
+                <Section class="chamada">
+                <!-- Chamada conteudo_chamada -->
+                    <div class="conteudo_chamada">
+                            <h2>Cadastre seu Passarinho e venha fazer parte desse banco de dados</h2>
+                            <p>Confira nosso sistema de cadastramente de passarinhos</p>
+                            <a href="" target="_blank">Cadastre Já</a>
+                        </div>
+                        <div class="conteudo_chamada--imagem">
+                            <img src="<?php echo get_theme_root_uri(); ?>/Tema-Passarinho/images/Passarinho topo.png" alt="Passarinhos">
+                        </div>
+                <!-- Chamada conteudo_chamada -->
+                </Section>
+                <!-- Chamada -->
+                <!-- Header -->  
+            </div>
+            <!-- Center -->
+            <!-- separacao -->
+            <div class="separacao">
+                &#8744;
+            </div>
+            <!-- separacao -->
+        </section>
+    <!-- topo-->
